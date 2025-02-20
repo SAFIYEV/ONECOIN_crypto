660fb52cfce25003dab74926a79dd03b70a4bc91
SAFIYEV <safievmarat65@gmail.com>
2025-02-20 12:53:40 +0300

    Add files via upload

--- assets/js/humb.js
+++ assets/js/humb.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.querySelector('.hamburger');
-  const navLinks = document.querySelector('.menu');
+  const navLinks = document.querySelector('.nav__links');
 
   hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('nav__active');
